42d46a0a64cc72f9644df3566d58bb227704e0ef
juanjo0824 <juandelgado1@colegioguanenta.edu.co>
2025-01-31 09:11:07 -0500

    actualizacion

--- Condicionales_1.py
+++ Condicionales_1.py
@@ -1,10 +1,12 @@
-cant_minutos = int(input("Digite la cantidad de minutos que duro la llamada ")) # input
+cant_minutos= input("digite la cantidad de minutos: ")
+cant_minutos= int(cant_minutos)
 
 if cant_minutos <= 3:
-    print("el valor de la llamada son 300 pesos")
+    valor_llama =300
+
 
 else:
-    valor_llamada = 300+50*(cant_minutos-3)
+    valor_llamada= 300+50*(cant_minutos-3)
 
 
-    print(f"el valor de la llamada son {valor_llamada}pesos")
+    print("el valor de la llamada es " + str(valor_llamada))
